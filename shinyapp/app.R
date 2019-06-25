a15c2b8e36b8620d1b2b548536470e1fe134bd4f
maxsaldana <mdsfalt@gmail.com>
2019-06-25 11:36:14 -0300

    trancada por prop

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1,32 +1,62 @@
 library(shiny)
 library(tidyverse)
-library(shinyWidgets)
+library(haven)
 
 datos <- read_dta("2011_2013panel.dta")
 dat_7 <- datos %>%
   select(form, año, g11, g12, g13, g14, g15)
 
 ui <- fluidPage(
-  selectInput(inputId = "var1",
-              ),
-  plotOutput("heatmap")
+  selectInput(inputId = "anio",
+              label = "Año",
+              c("2010" = "`1`","2011" = "`2`")
+  ),
+
   
+  selectInput(inputId = "var1",
+              label = "Variable en el eje x",
+              c("Tecnología" = "g11",
+                "Capacidad de producción" = "g12",
+                "Marketing/Comercialización" = "g13",
+                "Capacitación de personal" = "g14",
+                "Maquinarias y equipos" = "g15"
+              )
+  ),
+  
+  selectInput(inputId = "var2",
+               label = "Variable en el eje y",
+               c("Tecnología" = "g11",
+                 "Capacidad de producción" = "g12",
+                 "Marketing/Comercialización" = "g13",
+                 "Capacitación de personal" = "g14",
+                 "Maquinarias y equipos" = "g15"
+               )
+                   
+  ),
+ 
+   plotOutput("heatmap")
+
 )
 
 
 server <- function(input, output) {
+  
   output$heatmap <- renderPlot({
-    dat_7 %>%
-      filter(año == 1) %>%
-      group_by(g15, g12 ) %>%
+    
+    dat_plot <- reactive({
+      dat_7 %>%
+      filter(año == input$anio) %>%
+      group_by(input$var1, input$var2) %>%
       summarise(n = n()) %>%
-      mutate(prop = round(n/sum(n), 4)) %>%
-      ggplot(aes(x = as.factor(g15), y = as.factor(g12))) +
+      mutate(prop = n())
+    })
+  
+  ggplot(dat_plot(), aes(x = as.factor(input$var1), y = as.factor(input$var2))) +
       geom_tile(aes(fill = prop)) +
       geom_text(aes(label = prop), color = "white") +
       scale_x_discrete(labels = c("Mucho peor", "Peor", "Igual", "Mejor", "Mucho mejor")) +
       scale_y_discrete(labels = c("Mucho peor", "Peor", "Igual", "Mejor", "Mucho mejor")) +
-      labs(x = "Maquinaria y equipos", y = "Capacidad de producción", fill = "Proporción")
+      labs(x = "", y = "", fill = "Proporción")
   })
 }
 
