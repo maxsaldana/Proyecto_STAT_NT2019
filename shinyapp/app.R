e2385b9991700f11a7daeaace2cdc351b9528425
maxsaldana <mdsfalt@gmail.com>
2019-06-25 21:02:27 -0300

    app trancada

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -8,35 +8,47 @@ dat_7 <- datos %>%
   mutate(año = as.factor(recode(año, `1` = 2010, `2` = 2011)))
 
 ui <- fluidPage(
-  titlePanel(h3("Mapa de calor")),
-  selectInput(inputId = "anio",
-              label = "Año",
-              c("2010" = "2010","2011" = "2011")
+  titlePanel(h3("Mapa de calor", align = "center")),
+  
+  
+  plotOutput("heatmap"),
+  
+  hr(),
+  
+  fluidRow(
+  column(4,
+   selectInput(inputId = "anio",
+               label = "Año",
+               c("2010" = "2010","2011" = "2011")
+   )
   ),
 
-  
-  selectInput(inputId = "var1",
-              label = "Variable en el eje x",
-              c("Tecnología" = "g11",
-                "Capacidad de producción" = "g12",
-                "Marketing/Comercialización" = "g13",
-                "Capacitación de personal" = "g14",
-                "Maquinarias y equipos" = "g15"
-              )
-  ),
-  
-  selectInput(inputId = "var2",
-               label = "Variable en el eje y",
+  column(4,
+   selectInput(inputId = "var1",
+               label = "Variable en el eje x",
                c("Tecnología" = "g11",
                  "Capacidad de producción" = "g12",
                  "Marketing/Comercialización" = "g13",
                  "Capacitación de personal" = "g14",
                  "Maquinarias y equipos" = "g15"
-               )
-                   
+              )
+   )
   ),
+  
+  column(4,
+   selectInput(inputId = "var2",
+                label = "Variable en el eje y",
+                c("Tecnología" = "g11",
+                  "Capacidad de producción" = "g12",
+                  "Marketing/Comercialización" = "g13",
+                  "Capacitación de personal" = "g14",
+                  "Maquinarias y equipos" = "g15"
+                )
+   )                   
+  )
+)
  
-   plotOutput("heatmap")
+   
 
 )
 
@@ -50,7 +62,7 @@ server <- function(input, output) {
       filter(año == input$anio) %>%
       group_by(input$var1, input$var2) %>%
       summarise(n = n()) %>%
-      mutate(prop = n())
+      mutate(prop = n/sum(n))
     })
   
   ggplot(dat_plot(), aes(x = as.factor(input$var1), y = as.factor(input$var2))) +
